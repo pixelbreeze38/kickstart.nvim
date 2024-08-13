cef0e34126fc7d5c81df1e2f3f2e4a5acb2f664d
Rafael Sales de Jesus <rafael.jesus_meta@prestador.globo>
2024-08-13 04:54:47 -0300

    Refactored plugin structure and management

--- init.lua
+++ init.lua
@@ -80,17 +80,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
 -- Lazy.nvim Setup
 -- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
